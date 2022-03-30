6d93185fb019f927155bb699c54d07e23df88d45
Miguel <mj.rarycops@gmail.com>
2022-03-30 18:12:59 +0200

    update

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ async function main() {
                         });
                         break;
                 }
-            }
+            } 
         }
         else {
             let res = getAllFiles("./");
