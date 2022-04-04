678458911a4e80078dd2f7e840c0daaf029b91da
Miguel <mj.rarycops@gmail.com>
2022-04-04 16:07:48 +0200

    update

--- index.js
+++ index.js
@@ -117,8 +117,8 @@ async function main() {
                 //         break;
                 // }
                 switch (mapping_file_extension) {
-                    case 'rml.ttl':
-                    case 'rml.nt':
+                    case 'rml':
+                    case 'rml':
                         fle = fle.join('/').split('/').pop();
                         core.setOutput('run', true);
                         data = '\n\n[' + "mapping_file_" + fle + ']\nmappings=./' + file;
